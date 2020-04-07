e4567f64d8cc4bb37e9cbe7881595535a07ac2d4
ProBeta1 <abhinavkishan.56@gmail.com>
2020-04-07 18:54:22 +0530

    made responsive

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -66,14 +66,12 @@ class Meme extends React.Component {
     //console.log(this.state.newMemes);
 
     return (
-      <div className="bg-orange-300 border-4 border-purple-400 rounded-lg ml-16 hover:border-8 hover:border-purple-800 hover:rounded-full">
-        <div className="bg-blue-400 hover:bg-gray-400">
+      <div className="bg-orange-300 md:border-4 border-purple-400 rounded-sm md:rounded-lg ml-2 md:ml-8 lg:ml-16 hover:border-8 hover:border-purple-800 hover:rounded-full">
+        <div className="bg-blue-400 hover:bg-gray-400 max-w-sm sm:max-w-md md:max-w-lg max-h-sm sm:max-h-md md:max-h-lg ">
           {
             <img
               src={this.state.pictures}
               alt="Please refresh the page if no meme is showing up"
-              height="900px"
-              width="600px"
             />
           }
         </div>
